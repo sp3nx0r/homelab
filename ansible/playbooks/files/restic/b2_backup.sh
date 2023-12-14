a7ecab30fc6d8c410e42eaf011b7e8804e3c49fd
Spencer Koch <sp3nx0r@gmail.com>
2023-12-14 15:20:45 -0600

    fix: restic target directories were incorrect

--- ansible/playbooks/files/restic/b2_backup.sh
+++ ansible/playbooks/files/restic/b2_backup.sh
@@ -4,5 +4,6 @@ source /etc/restic/b2.env
 
 sqlite3 "${VAULTWARDEN_DIR}"/db.sqlite3 ".backup ${VAULTWARDEN_DIR}/db.sqlite3.backup"
 restic --limit-upload 2000 backup /storage/backup /storage/docker /storage/plex-data
-restic --limit-upload 2000 backup /storage-fast/tools-vaultwarden-config
+restic --limit-upload 2000 backup "${VAULTWARDEN_DIR}"
+restic --limit-upload 2000 backup /storage/datastore-minio/tfstate
 restic --verbose --keep-last 5 --keep-daily 6 --keep-weekly 3 forget
